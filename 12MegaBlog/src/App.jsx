2609f5e379ae80dc4965deafad5e9220de8d6f57
suman <sumanequasar@gmail.com>
2025-06-09 18:10:49 +0530

    megablog m auth pages and other

--- 12MegaBlog/src/App.jsx
+++ 12MegaBlog/src/App.jsx
@@ -1,11 +1,34 @@
+import { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+import authService from "./appwrite/auth";
+import { login, logout } from "./store/authSlice";
 import "./App.css";
+import { Header, Footer } from "./components";
 
 function App() {
-  return (
-    <>
-      <h1> A blog app with appwrite</h1>
-    </>
-  );
-}
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    authService
+      .getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }));
+        } else {
+          dispatch(logout());
+        }
+      })
+      .finally(() => setLoading(false));
+  }, []);
 
+  return !loading ? (
+    <div className="min-h-screen flex flex-wrap  content-betwwwn bg-gray-400">
+      <div className="w-full block">
+        <Header />
+        <main>Todo: {/* <Outlet/> */}</main>
+        <Footer />{" "}
+      </div>
+    </div>
+  ) : null;
+}
 export default App;
---
+++ 12MegaBlog/src/components/Input.jsx
@@ -0,0 +1,7 @@
+import React, { useId } from "react";
+
+const Input = React.forwardRef(function Input({ label, type = "text" }) {
+  return <h1> Test</h1>;
+});
+
+export default Input;
